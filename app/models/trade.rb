ff191d5bc447472261d7b1f303ec48aa94dce1ec
Jean Felipe <jean-felipeds@hotmail.com>
2018-02-24 02:01:30 -0300

    docker installed success

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: trades
+#
+#  id          :integer          not null, primary key
+#  trande_type :integer
+#  date        :datetime
+#  price       :decimal(, )
+#  cpf         :string
+#  card_number :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Trade < ApplicationRecord
 end
